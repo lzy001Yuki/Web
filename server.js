3c258c5258039f32a7414e6bca7088cb06d4dc1e
Yuki <lzy001@sjtu.edu.cn>
2025-05-23 12:45:57 +0800

    feat: refator

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "")));
 
 // 模拟一个静态用户数据
 const users = [
